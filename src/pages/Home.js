ad74c18316edde4021d33ac16047f7c12e7b31f1
Spencer Stissi <sas7359@g.rit.edu>
2019-10-28 08:16:34 -0400

    Fixed alignment/scaling of images on home page to function correctly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { Container, Col, Row, Image, Button } from 'react-bootstrap';
+import { isAbsolute } from 'path';
+
+// Imline styling stuff cause too lazy to make css file :)
+const styles = {
+    nopadding: {
+        paddingLeft: 0,
+        paddingRight: 0
+    },
+};
 
 class Home extends React.Component {
 
@@ -12,13 +21,13 @@ class Home extends React.Component {
                 <Container>
                     <div>
                         <Row className="justify-content-md-center">
-                            <Col sm={4}><Image src="MemeMakerHome_1.png" height="250px" width="250px" fluid/></Col>
-                            <Col sm={4}><Image src="peepoClown.png" fluid/></Col>
+                            <Col sm={4} style={styles.nopadding}><Image src="MemeMakerHome_1.png" fluid/></Col>
+                            <Col sm={4} className="align-self-end" style={styles.nopadding}><Image src="MemeMakerHome_3.png" fluid/></Col>
                         </Row>
                         <Row className="justify-content-md-center">
-                            <Col sm={4}><Image src="MemeMakerHome_2.png" fluid/></Col>
-                            <Col sm={4}><Image src="ScaredPatrickMeme.jpg" height="250px" width="250px" fluid/></Col>
-                            <Col sm={4}><Image src="UgandaMeme.jpg" height="250px" width="250px" fluid/></Col>
+                            <Col sm={4} style={styles.nopadding}><Image src="MemeMakerHome_2.png"  fluid/></Col>
+                            <Col sm={3} style={styles.nopadding}><Image src="UgandaMeme.jpg" fluid/></Col>
+                            <Col sm={3} style={styles.nopadding}><Image src="ScaredPatrickMeme.jpg" fluid/></Col>
                         </Row>
                         <br />
                         <Row className="justify-content-md-center">
