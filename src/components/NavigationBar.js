a49e92d546ca17dcdc6b2b857e9f4e21584b2ede
sas7359 <sas7359@g.rit.edu>
2019-11-28 15:15:05 -0500

    Made font size and boldness of navigation bar items more legible

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Navbar, Nav, Image } from 'react-bootstrap';
 
+const navItemStyle = {
+    fontSize: 20,
+    fontWeight: "bold"
+}
+
 class NavigationBar extends React.Component {
 
     render() {
         return (
             <Navbar bg="light" variant="light">
                 <Nav className="mr-auto">
-                    <Nav.Item><Nav.Link href="/">
+                    <Nav.Item><Nav.Link href="/" style={navItemStyle}>
                         <i className="fa fa-home" aria-hidden="true" /> Home
                     </Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Editor">
+                    <Nav.Item><Nav.Link href="/Editor" style={navItemStyle}>
                         <i className="fa fa-pencil" aria-hidden="true" /> Meme Editor
                     </Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Library">
+                    <Nav.Item><Nav.Link href="/Library" style={navItemStyle}>
                         <i className="fa fa-book" aria-hidden="true" /> Template Library
                     </Nav.Link></Nav.Item>
                 </Nav>
